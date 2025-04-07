caa253ea5091fa4d7f30d72905bb5c51375e8e61
hmuto111 <h.muto111@gmail.com>
2025-04-07 16:23:55 +0900

    feat: ヘッダーの実装 #2

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router";
+import { Header } from "@/components/header/header";
 
 import { paths } from "@/config/paths";
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>Home</h1>
       <Link to={paths.app.population_graph.getHref()}>Population Graph</Link>
     </div>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,24 @@
+import "./header.css";
+import { Link } from "react-router";
+
+import { paths } from "@/config/paths";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <Link to={paths.app.population_graph.getHref()} className="app-name">
+        popu
+      </Link>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to={paths.home.path}>Home</Link>
+          </li>
+          <li>
+            <Link to={paths.app.population_graph.getHref()}>graph</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
